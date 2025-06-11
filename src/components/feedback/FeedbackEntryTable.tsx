f47afef8b670abb45885a24cd52c0ffa9cd80c0d
diggory-rycroft <diggory.rycroft@vercel.com>
2025-06-11 16:07:50 -0400

    before tablecn

---
+++ src/components/feedback/FeedbackEntryTable.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+interface FeedbackEntryTableProps {
+  // Add props as needed
+}
+
+export function FeedbackEntryTable({}: FeedbackEntryTableProps) {
+  return (
+    <div>
+      {/* Component content will go here */}
+    </div>
+  );
+} 
