12897d681cf1ea962f6ef02d902ec40089b3f087
diggory-rycroft <diggory.rycroft@vercel.com>
2025-06-13 14:25:13 -0400

    gradient change

--- src/app/feedback/[slug]/page.tsx
+++ src/app/feedback/[slug]/page.tsx
@@ -66,7 +66,7 @@ export default async function FeedbackItemPage({
             trigger={
               <button
                 type="button"
-                className="inline-flex items-center gap-x-2.5 rounded-xl bg-gradient-to-r from-orange-200 to-emerald-400 px-6 py-3.5 text-lg font-semibold text-black shadow-[0_4px_24px_0_rgba(0,0,0,0.12)] hover:shadow-[0_8px_32px_0_rgba(0,0,0,0.16)] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black transition-all duration-200 hover:scale-[1.03] cursor-pointer"
+                className="inline-flex items-center gap-x-2.5 rounded-xl bg-gradient-to-r from-teal-200 to-teal-500 px-6 py-3.5 text-lg font-semibold text-slate-800 shadow-[0_4px_24px_0_rgba(0,0,0,0.12)] hover:shadow-[0_8px_32px_0_rgba(0,0,0,0.16)] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black transition-all duration-200 hover:scale-[1.03] cursor-pointer"
               >
                 <svg className="h-6 w-6" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                   <path d="M10.75 4.75a.75.75 0 00-1.5 0v4.5h-4.5a.75.75 0 000 1.5h4.5v4.5a.75.75 0 001.5 0v-4.5h4.5a.75.75 0 000-1.5h-4.5v-4.5z" />
