3db53d143be417ea89064f07f18754c3a235cf40
diggory-rycroft <diggory.rycroft@vercel.com>
2025-06-30 15:58:37 -0400

    performance improvements

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -19,13 +19,11 @@ export default function Template({ children }: TemplateProps) {
 
   return (
     <div 
-      className={`transition-all duration-150 ease-in-out ${
-        isVisible 
-          ? 'opacity-100 translate-y-0' 
-          : 'opacity-0 translate-y-2'
+      className={`transition-opacity duration-150 ease-in-out ${
+        isVisible ? 'opacity-100' : 'opacity-90'
       }`}
       style={{
-        transitionProperty: 'opacity, transform',
+        transitionProperty: 'opacity',
         transitionDuration: '150ms',
         transitionTimingFunction: 'ease-in-out'
       }}
